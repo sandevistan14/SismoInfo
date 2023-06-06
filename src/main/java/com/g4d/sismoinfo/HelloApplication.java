2fcfcdee4eaf7a95a76eed16c753a0165703a437
MaximeJAURASJ22012943 <maxime.jauras@etu.univ-amu.fr>
2023-06-06 21:56:11 +0200

    Creation of a Borderpane containing a MenuBar with 4 Menu located at the top. Addition of a button which will be used to add a csv file. (--> home page)

--- src/main/java/com/g4d/sismoinfo/HelloApplication.java
+++ src/main/java/com/g4d/sismoinfo/HelloApplication.java
@@ -25,7 +25,7 @@ public class HelloApplication extends Application {
     
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/g4d/sismoinfo/hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
