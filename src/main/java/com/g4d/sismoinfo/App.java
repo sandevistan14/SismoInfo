81376e123e91457eb66dc1eff7dec45b6baa3256
MaximeJAURASJ22012943 <maxime.jauras@etu.univ-amu.fr>
2023-06-08 18:15:40 +0200

    Change of page when clicking on the menus.

--- src/main/java/com/g4d/sismoinfo/App.java
+++ src/main/java/com/g4d/sismoinfo/App.java
@@ -27,7 +27,7 @@ public class App extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/com/g4d/sismoinfo/home.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Recherche sismiques");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/g4d/sismoinfo/MapController.java
+++ src/main/java/com/g4d/sismoinfo/MapController.java
@@ -1,5 +1,39 @@
 package com.g4d.sismoinfo;
 
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.MenuItem;
+import javafx.stage.Stage;
+
 public class MapController {
-    
+    @FXML
+    private void handleMenuAction(ActionEvent event) throws IOException {
+        MenuItem menuItem = (MenuItem) event.getSource();
+        String menuText = menuItem.getText();
+
+        Stage stage = (Stage) menuItem.getParentPopup().getOwnerWindow();
+
+        if (menuText.equals("Accueil")) {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/g4d/sismoinfo/home.fxml"));
+            Parent homeRoot = fxmlLoader.load();
+            Scene scene = new Scene(homeRoot, 320, 240);
+            stage.setScene(scene);
+        } else if (menuText.equals("Carte")) {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/g4d/sismoinfo/map.fxml"));
+            Parent mapRoot = fxmlLoader.load();
+            Scene scene = new Scene(mapRoot, 320, 240);
+            stage.setScene(scene);
+        } else if (menuText.equals("Dashboard")) {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/g4d/sismoinfo/dashboard.fxml"));
+            Parent dashboardRoot = fxmlLoader.load();
+            Scene scene = new Scene(dashboardRoot, 320, 240);
+            stage.setScene(scene);
+        }
+        stage.show();
+    }
 }
