aea6f0c970233fc7f0d9fdb3f00463a6739237a9
Elliot Lartigaud <92160866+MrPoulet9987@users.noreply.github.com>
2023-06-12 14:54:35 +0200

    JavaDoc package com.g4d.sismoinfo.model

--- src/main/java/com/g4d/sismoinfo/model/earthquakedata/Shock.java
+++ src/main/java/com/g4d/sismoinfo/model/earthquakedata/Shock.java
@@ -1,5 +1,8 @@
 package com.g4d.sismoinfo.model.earthquakedata;
 
+/**
+ * The `Shock` enum represents the types of shocks associated with an earthquake.
+ */
 public enum Shock {
     BLANK(""),
     P("PRECURSEUR"),
@@ -9,14 +12,30 @@ public enum Shock {
 
     private final String value;
 
+    /**
+     * Constructs a `Shock` enum value with the given shock type.
+     *
+     * @param shock The shock type.
+     */
     Shock(String shock) {
         this.value = shock;
     }
 
+    /**
+     * Gets the string value of the shock type.
+     *
+     * @return The string value of the shock type.
+     */
     public String getValue() {
         return value;
     }
 
+    /**
+     * Retrieves the `Shock` enum value based on the given shock type value.
+     *
+     * @param givenName The shock type value.
+     * @return The corresponding `Shock` enum value, or `null` if not found.
+     */
     public static Shock fromValue(String givenName) {
         for (Shock shock : values()) {
             if (shock.value.equals(givenName)) {
--- src/main/java/com/g4d/sismoinfo/model/earthquakedata/epicentralCode.java
+++ src/main/java/com/g4d/sismoinfo/model/earthquakedata/epicentralCode.java
@@ -1,5 +1,8 @@
 package com.g4d.sismoinfo.model.earthquakedata;
 
+/**
+ * The `epicentralCode` enum represents the quality codes for epicentral intensity.
+ */
 public enum epicentralCode {
     A("SURE"),
     B("ASSEZ SURE"),
@@ -11,14 +14,30 @@ public enum epicentralCode {
 
     private final String value;
 
+    /**
+     * Constructs a `epicentralCode` enum value with the given code.
+     *
+     * @param epicentralCode The code value for the epicentral intensity quality.
+     */
     epicentralCode(String epicentralCode) {
         this.value = epicentralCode;
     }
 
+    /**
+     * Gets the string value of the epicentral intensity quality code.
+     *
+     * @return The string value of the epicentral intensity quality code.
+     */
     public String getValue() {
         return value;
     }
 
+    /**
+     * Retrieves the `epicentralCode` enum value based on the given code value.
+     *
+     * @param givenName The code value for the epicentral intensity quality.
+     * @return The corresponding `epicentralCode` enum value, or `null` if not found.
+     */
     public static epicentralCode fromValue(String givenName) {
         for (epicentralCode code : values()) {
             if (code.value.equals(givenName)) {
